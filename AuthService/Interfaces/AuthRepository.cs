f35704929d18d166cc9c81a884b97d235544c15b
Siddhesh Vetal <siddheshvetal676@gmail.com>
2026-03-18 19:22:39 +0530

    sp fix

--- AuthService/Interfaces/AuthRepository.cs
+++ AuthService/Interfaces/AuthRepository.cs
@@ -19,36 +19,36 @@ public class AuthRepository : IAuthRepository
     public async Task<int> RegisterUserAsync(User user)
     {
         using var conn = CreateConnection();
-        return await conn.ExecuteScalarAsync<int>(
-            "\"usp_RegisterUser\"",  // CHANGED: Added double quotes
-            new
-            {
-                p_FullName = user.FullName,
-                p_Email = user.Email,
-                p_PasswordHash = user.PasswordHash,
-                p_Role = user.Role
-            },
-            commandType: System.Data.CommandType.StoredProcedure
-        );
+
+        // Use raw SQL with CALL statement instead of StoredProcedure type
+        var sql = "CALL \"usp_RegisterUser\"(@p_FullName, @p_Email, @p_PasswordHash, @p_Role, null)";
+
+        var parameters = new
+        {
+            p_FullName = user.FullName,
+            p_Email = user.Email,
+            p_PasswordHash = user.PasswordHash,
+            p_Role = user.Role
+        };
+
+        await conn.ExecuteAsync(sql, parameters);
+
+        // Get the last inserted ID
+        var userId = await conn.ExecuteScalarAsync<int>("SELECT LASTVAL();");
+        return userId;
     }
 
     public async Task<User?> GetUserByEmailAsync(string email)
     {
         using var conn = CreateConnection();
-        return await conn.QueryFirstOrDefaultAsync<User>(
-            "\"usp_GetUserByEmail\"",  // CHANGED: Added double quotes
-            new { p_Email = email },
-            commandType: System.Data.CommandType.StoredProcedure
-        );
+        var sql = "SELECT * FROM \"Users\" WHERE \"Email\" = @p_Email";
+        return await conn.QueryFirstOrDefaultAsync<User>(sql, new { p_Email = email });
     }
 
     public async Task<User?> GetUserByIdAsync(int userId)
     {
         using var conn = CreateConnection();
-        return await conn.QueryFirstOrDefaultAsync<User>(
-            "\"usp_GetUserById\"",
-            new { p_UserId = userId },
-            commandType: System.Data.CommandType.StoredProcedure
-        );
+        var sql = "SELECT * FROM \"Users\" WHERE \"UserId\" = @p_UserId";
+        return await conn.QueryFirstOrDefaultAsync<User>(sql, new { p_UserId = userId });
     }
 }
